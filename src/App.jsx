394452e96c133ecb21e515e51a242500251a7e86
ppisland <a1365gkqls@gmail.com>
2023-04-05 14:31:25 +0900

    effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const onClickToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      {toggle && <Counter count={count} setCount={setCount} />}
+      {/* {toggle ? <Counter /> : "카운터"} */}
+      <button
+        className="bg-pink-300 px-4 py-2 rounded-lg text-white font-bold mt-4"
+        onClick={onClickToggle}
+      >
+        버튼
+      </button>
+    </div>
+  );
 }
 
 export default App;
